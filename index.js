12661d8fb3db8f15fa7fbfc1f4aa7dd560a86e51
andreasonny83 <andreasonny83@gmail.com>
2017-05-14 14:18:14 +0100

    first working test

---
+++ index.js
@@ -0,0 +1,59 @@
+const nodemailer = require('nodemailer');
+const app = require('express')();
+const router = require('express').Router();
+const port = process.env.PORT || 8009;
+
+const host = process.env.HOST_NAME;
+const portNo = process.env.PORT_NUMBER;
+const user = process.env.USER_NAME;
+const pass = process.env.PASSWORD;
+const secure = process.env.SECURE || false;
+
+app.use('/send', router);
+
+router.get('/', sendEmail); // handle the route at yourdomain.com/sayHello
+
+function sendEmail(req, res) {
+  // Not the movie transporter!
+  const transporter = nodemailer.createTransport({
+    host: host,
+    port: portNo,
+    secure: secure,
+    secureConnection: secure,
+    auth: {
+      user: user, // Your email id
+      pass: pass // Your password
+    },
+    tls: {
+      rejectUnauthorized: secure
+    }
+  });
+
+  var mailOptions = {
+    from: '"Our Code World " <noreply@sonnywebdesign.com>', // sender address (who sends)
+    to: 'andreasonny83@gmail.com', // list of receivers
+    subject: 'Hello', // Subject line
+    text: 'Hello world ', // plaintext body
+    html: '<b>Hello world </b><br> This is the first email sent with Nodemailer in Node.js' // html body
+  };
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if(error) {
+      console.log(error);
+      res.json({yo: 'error'});
+    } else {
+      console.log('Message sent: ' + info.response);
+      res.json({yo: info.response});
+    };
+  });
+}
+
+app.listen(port, function() {
+  console.log(`Express server is listening on http://localhost:${port}`);
+
+  console.log([
+    'env = ' + app.get('env'),
+    '__dirname = ' + __dirname,
+    'process.cwd = ' + process.cwd()
+  ].join('\n'));
+});
